2e990bb8d23b1497b9a5be6585cd0037b32698fc
Noah Muscat <noahmu@umich.edu>
2024-09-17 11:15:34 -0400

    file path switch

--- data/Analysis/CombinedRats/CircadianAnalysis.m
+++ data/Analysis/CombinedRats/CircadianAnalysis.m
@@ -3,7 +3,7 @@
 %% Reading in data
 % reading in data, already in ZT form 
 convertZT = false;
-combined_data = readtable('/Users/noahmuscat/University of Michigan Dropbox/Noah Muscat/JeremyAnalysis/ZT/Combined_Normalized_Data_With_RelativeDays.csv');
+combined_data = readtable('/Users/noahmuscat/University of Michigan Dropbox/Noah Muscat/JeremyAnalysis/ZT/CombinedRelDaysBinned.csv');
 %% Wrangling hourly bins
 [hourlyMeans, hourlyBinTimes] = CalculateHourlyMeans(combined_data);
 
