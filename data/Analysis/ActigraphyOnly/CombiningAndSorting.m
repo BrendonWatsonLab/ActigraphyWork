99cc971b50ef77805891ea007644eeb2758c77db
Noah Muscat <noahmu@umich.edu>
2024-10-22 17:59:05 -0400

    Adding script for combining AO1-4

---
+++ data/Analysis/ActigraphyOnly/CombiningAndSorting.m
@@ -0,0 +1,11 @@
+%% Combining and Sorting for Actigraphy-Only Animals
+
+% AO1-4
+CombineSortZT('/nfs/turbo/umms-brendonw/JeremyData/GrassRatActigraphyOnlyData/Cohort1Data','AO1_300Lux', 5);
+CombineSortZT('/nfs/turbo/umms-brendonw/JeremyData/GrassRatActigraphyOnlyData/Cohort1Data','AO1_1000Lux', 5);
+CombineSortZT('/nfs/turbo/umms-brendonw/JeremyData/GrassRatActigraphyOnlyData/Cohort1Data','AO2_300Lux', 5);
+CombineSortZT('/nfs/turbo/umms-brendonw/JeremyData/GrassRatActigraphyOnlyData/Cohort1Data','AO2_1000Lux', 5);
+CombineSortZT('/nfs/turbo/umms-brendonw/JeremyData/GrassRatActigraphyOnlyData/Cohort1Data','AO3_300Lux', 5);
+CombineSortZT('/nfs/turbo/umms-brendonw/JeremyData/GrassRatActigraphyOnlyData/Cohort1Data','AO3_1000Lux', 5);
+CombineSortZT('/nfs/turbo/umms-brendonw/JeremyData/GrassRatActigraphyOnlyData/Cohort1Data','AO4_300Lux', 5);
+CombineSortZT('/nfs/turbo/umms-brendonw/JeremyData/GrassRatActigraphyOnlyData/Cohort1Data','AO4_1000Lux', 5);
