47c79e347350f989e76464dba07b8bba603faaba
Noah Muscat <noahmu@umich.edu>
2024-10-23 08:57:46 -0400

    Adding all files for AO1-8

--- data/Analysis/ActigraphyOnly/SuperCombine.m
+++ data/Analysis/ActigraphyOnly/SuperCombine.m
@@ -1,6 +1,6 @@
 %% Combining all rat data into one file
 
-rootFolder = '/home/Documents/JeremyData/ActigraphyOnly';
+rootFolder = '/home/noahmu/Documents/JeremyData/ActigraphyOnly';
 
 animals = {'AO1', 'AO2', 'AO3', 'AO4', 'AO5', 'AO6', 'AO7', 'AO8'};
 
