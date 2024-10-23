5fac9405f6a8ff2e95a045cc37e2f3a09c14e88f
Noah Muscat <noahmu@umich.edu>
2024-10-22 21:12:54 -0400

    adding stuff to combine all AO files

---
+++ data/Analysis/ActigraphyOnly/SuperCombine.m
@@ -0,0 +1,9 @@
+%% Combining all rat data into one file
+
+rootFolder = '/home/Documents/JeremyData/ActigraphyOnly';
+
+animals = {'AO1', 'AO2', 'AO3', 'AO4', 'AO5', 'AO6', 'AO7', 'AO8'};
+
+conditions = {'300Lux', '1000Lux'};
+
+combined_data_total = Normalize_Combine(rootFolder, animals, conditions);
