d1ebd08c63f69110b577638a7b3b81cda3dd3b6c
Noah Muscat <noahmu@umich.edu>
2024-10-25 19:46:06 -0400

    Preparing for AO1-8 analysis

--- data/Analysis/ActigraphyOnly/SuperCombine.m
+++ data/Analysis/ActigraphyOnly/SuperCombine.m
@@ -1,6 +1,6 @@
 %% Combining all rat data into one file
 
-rootFolder = '/home/noahmu/Documents/JeremyData/ActigraphyOnly';
+rootFolder = '/data/Jeremy/NoahJeremySharedFolder/Data_Files_Actig_Only';
 
 animals = {'AO1', 'AO2', 'AO3', 'AO4', 'AO5', 'AO6', 'AO7', 'AO8'};
 
@@ -9,11 +9,11 @@ conditions = {'300Lux', '1000Lux'};
 combined_data_total = Normalize_Combine(rootFolder, animals, conditions);
 
 %% Adding relative days
-combined_data_reldays = RelativeDayCalculator('/home/noahmu/Documents/JeremyData/ActigraphyOnly');
+combined_data_reldays = RelativeDayCalculator('/data/Jeremy/NoahJeremySharedFolder/Data_Files_Actig_Only');
 
 %% Binning data
 
-binned_data = Binner('/home/noahmu/Documents/JeremyData/ActigraphyOnly/Combined_Normalized_Data_With_RelativeDays.csv', 5, true);
+binned_data = Binner('/data/Jeremy/NoahJeremySharedFolder/Data_Files_Actig_Only/Combined_Normalized_Data_With_RelativeDays.csv', 5, true);
 
 %% Cleaning data
 
